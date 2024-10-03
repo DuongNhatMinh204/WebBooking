6db5ec53a69d4aed2f5471cb9010284dddbd0bc7
DuongNhatMinh204 <nhatminh.st.04@gmail.com>
2024-10-03 11:56:59 +0700

    update

---
+++ src/main/java/com/java/web_travel/controller/AdminController.java
@@ -0,0 +1,20 @@
+package com.java.web_travel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class AdminController {
+    @RequestMapping(value = "admin_booking" , method = RequestMethod.GET)
+    public ModelAndView adminBooking(){
+        ModelAndView mav = new ModelAndView("admin_booking");
+        return mav ;
+    }
+    @RequestMapping(value = "admin_account" , method = RequestMethod.GET)
+    public ModelAndView adminAccount(){
+        ModelAndView mav = new ModelAndView("admin_account");
+        return mav ;
+    }
+}
--- src/main/java/com/java/web_travel/controller/UserController.java
+++ src/main/java/com/java/web_travel/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.java.web_travel.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class BookingController {
+public class UserController {
     @RequestMapping(value = "booking" , method = RequestMethod.GET)
     public ModelAndView booking() {
         ModelAndView mav = new ModelAndView("booking");
